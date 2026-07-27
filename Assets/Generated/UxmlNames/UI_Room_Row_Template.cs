aa455f49940de998c7be2e6df42780f6773961ee
Beru <lidor.y.shapira@gmail.com>
2026-07-27 20:10:20 +0300

    added map object

--- Assets/Generated/UxmlNames/UI_Room_Row_Template.cs
+++ Assets/Generated/UxmlNames/UI_Room_Row_Template.cs
@@ -1,6 +1,6 @@
 // AUTO-GENERATED — DO NOT EDIT
 // Source : Assets/UI Toolkit/UXMLs/Templates/Room_Row_Template.uxml
-// Updated: 2026-07-27 16:14 UTC
+// Updated: 2026-07-27 17:00 UTC
 
 using System.Collections.Generic;
 
