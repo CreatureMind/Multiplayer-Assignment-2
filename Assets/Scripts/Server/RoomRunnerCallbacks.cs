85328b9a422aa5167e676b65e27e4b2ca0114b32
TheFull0 <196220277+TheFull0@users.noreply.github.com>
2026-07-28 04:19:56 +0300

    on matchstarted event

--- Assets/Scripts/Server/RoomRunnerCallbacks.cs
+++ Assets/Scripts/Server/RoomRunnerCallbacks.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Events;
 using Fusion;
 using Fusion.Sockets;
 using UnityEngine;
@@ -26,6 +27,7 @@ public class RoomRunnerCallbacks : MonoBehaviour, INetworkRunnerCallbacks
     private bool _ownerAssigned;
     private PlayerRef _pendingOwner;
     private bool _hasPendingOwner;
+    private bool _matchStarted;
 
     private readonly Dictionary<PlayerRef, PlayerData> _players = new();
 
@@ -72,11 +74,15 @@ public class RoomRunnerCallbacks : MonoBehaviour, INetworkRunnerCallbacks
     public void SetController(RoomController controller)
     {
         _roomController = controller;
+        EventBus.Subscribe<MatchStartedEvent>(OnMatchStartedEvent);
         TryAssignPendingOwner();
     }
-
-    public bool IsMatchStarted => _roomController && _roomController.MatchStarted;
-
+    
+    private void OnMatchStartedEvent(MatchStartedEvent evt)
+    {
+        _matchStarted = true;
+    }
+    
     public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
     {
         if (!runner.IsServer) return;
@@ -151,7 +157,7 @@ public class RoomRunnerCallbacks : MonoBehaviour, INetworkRunnerCallbacks
 
     public void OnSceneLoadDone(NetworkRunner runner)
     {
-        var isPostMatchLoad = IsMatchStarted;
+        var isPostMatchLoad = _matchStarted;
         Debug.Log($"[Server] Room '{_sessionName}' OnSceneLoadDone: {runner.SceneManager.MainRunnerScene.name}, postMatchLoad={isPostMatchLoad}.");
         _manager.TryBeginPostGameSceneBootstrap(_roomId, runner, isPostMatchLoad);
     }
