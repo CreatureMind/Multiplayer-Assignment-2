edbc9b63187d48c2ed276feb82ab78af249a3e54
TheFull0 <196220277+TheFull0@users.noreply.github.com>
2026-07-28 05:40:51 +0300

    try again

--- Assets/Generated/UxmlNames/UI_In_Game_View.cs
+++ Assets/Generated/UxmlNames/UI_In_Game_View.cs
@@ -1,6 +1,6 @@
 // AUTO-GENERATED — DO NOT EDIT
 // Source : Assets/UI Toolkit/UXMLs/Views/In_Game_View.uxml
-// Updated: 2026-07-28 01:14 UTC
+// Updated: 2026-07-28 02:19 UTC
 
 using System.Collections.Generic;
 
