51edabbe1b93bfc0350d0c85c596b7b599643244
CreatureMind <umarovart@gmail.com>
2026-08-07 22:12:05 +0300

    Added credits view

---
+++ Assets/Generated/UxmlNames/UI_Credits_View.cs
@@ -0,0 +1,31 @@
+// AUTO-GENERATED — DO NOT EDIT
+// Source : Assets/UI Toolkit/UXMLs/Views/Credits_View.uxml
+// Updated: 2026-08-07 19:11 UTC
+
+using System.Collections.Generic;
+
+public enum UI_Credits_View
+{
+    tint,
+    container,
+    panel,
+    header,
+    Label,
+    bottom_container,
+    back_button,
+}
+
+internal static class UI_Credits_View_Map
+{
+    internal static readonly Dictionary<UI_Credits_View, string> Names =
+        new Dictionary<UI_Credits_View, string>
+        {
+            { UI_Credits_View.tint, "tint" },
+            { UI_Credits_View.container, "container" },
+            { UI_Credits_View.panel, "panel" },
+            { UI_Credits_View.header, "header" },
+            { UI_Credits_View.Label, "Label" },
+            { UI_Credits_View.bottom_container, "bottom-container" },
+            { UI_Credits_View.back_button, "back-button" },
+        };
+}
