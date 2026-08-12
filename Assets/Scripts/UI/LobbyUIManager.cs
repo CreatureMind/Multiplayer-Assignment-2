9950f80231b76361ea0ddf4dd1da1454301e6ddc
CreatureMind <umarovart@gmail.com>
2026-08-12 03:48:49 +0300

    maybe a fix for the bug on itch

--- Assets/Scripts/UI/LobbyUIManager.cs
+++ Assets/Scripts/UI/LobbyUIManager.cs
@@ -206,10 +206,12 @@ namespace UI
             // If name is already confirmed (or returning from match), go to Rooms list directly
             if (NetworkManager.Instance && NetworkManager.Instance.IsReturningFromMatch)
             {
+                HideAllScreens();
                 ShowScreen(LobbyScreen.None);
             }
             else
             {
+                HideAllScreens();
                 ShowScreen(LobbyScreen.MainMenu);
             }
         }
